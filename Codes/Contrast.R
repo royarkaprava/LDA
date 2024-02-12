1ebe50702efcc108c2ec9b968142111a64d9fe7e
royarkaprava <arkapravaroy0075@gmail.com>
2024-02-12 14:16:04 -0500

    Update Contrast.R

--- Codes/Contrast.R
+++ Codes/Contrast.R
@@ -31,3 +31,88 @@ summary(lm(measurement ~ Week, data=data_new))
 
 library(nlme)
 summary(lme(measurement ~ Week, random = ~1|Subject, data = data_new))
+
+
+#######################Contrast############################
+res <- lmer(measurement ~ factor(Week) + (1|Subject), data = data_new)
+coef(summary(res))  
+
+library(emmeans)
+library(ggplot2)
+
+res.emm <- emmeans::emmeans(res, "Week")
+
+#If we wanted to compare each visit against every other visit (i.e. all the pairwise comparisons) we can use
+emmeans::contrast(res.emm, 'tukey') %>%
+  broom::tidy() %>%
+  head
+
+
+# To check the polynomial contrasts, to see if there was a linear or quadratic change over time:
+emmeans::contrast(res.emm, 'poly') %>%
+  broom::tidy() %>%
+  head(3) 
+
+##################With interaction#######################
+
+res <- lmer(measurement ~ factor(Week)*Treatment + (1|Subject), data = data_new)
+coef(summary(res))  
+
+library(emmeans)
+library(ggplot2)
+
+res.emm <- emmeans::emmeans(res, "Week")
+
+#If we wanted to compare each time against every other times (i.e. all the pairwise comparisons) we can use
+emmeans::contrast(res.emm, 'tukey') %>%
+  broom::tidy() %>%
+  head
+
+
+# To check the polynomial contrasts, to see if there was a linear or quadratic change over time:
+emmeans::contrast(res.emm, 'poly') %>%
+  broom::tidy() %>%
+  head(3) 
+
+res.emm <- emmeans::emmeans(res, "Treatment")
+
+#If we wanted to compare the treatments (i.e. all the pairwise comparisons) we can use
+emmeans::contrast(res.emm, 'tukey') %>%
+  broom::tidy() %>%
+  head
+
+#####################For your custom constrast##########################
+res <- lmer(measurement ~ factor(Week)*Treatment + (1|Subject), data = data_new)
+
+## set up contrast (linear comb. of coefficients)
+ct <- c(0,0,0,0,0,1,1,1/2)   #This is the contrast from AUCMB_1-AUCMB_0 assuming Week 1 as time 1  
+
+m <- sum(fixef(res) * ct)  ## mean of contrast
+v <- t(ct) %*% vcov(res) %*% ct   ## variance of contrast
+stder <- sqrt(as.numeric(v))      ## standard error
+tstat <- m/stder      ## t statistic
+
+##########For large sample##################
+2*pnorm(abs(tstat), lower.tail=FALSE)
+#############For small sample##############
+number_of_parameters_estimated <- length(fixef(res)) + length(res@theta)  #number of parameters for mean + number of parameters for random effect variance
+error_df = nrow(data_new) - number_of_parameters_estimated
+2*pt(abs(tstat), df=error_df, lower.tail = FALSE)
+
+#Repeat the above for lme#################################################
+res <- lme(measurement ~ factor(Week)*Treatment, random = ~1|Subject, data = data_new)
+## set up contrast (linear comb. of coefficients)
+ct <- c(0,0,0,0,0,1,1,1/2)   #This is the contrast from AUCMB_1-AUCMB_0 assuming Week 1 as time 1  
+
+m <- sum(fixef(res) * ct)  ## mean of contrast
+v <- t(ct) %*% vcov(res) %*% ct   ## variance of contrast
+stder <- sqrt(as.numeric(v))      ## standard error
+tstat <- m/stder      ## t statistic
+
+##########For large sample##################
+2*pnorm(abs(tstat), lower.tail=FALSE)
+#############For small sample##############
+number_of_parameters_estimated <- length(fixef(res)) + sum(upper.tri(getVarCov(res,type = c("random.effects")), diag=T))  #number of parameters for mean + number of parameters for variance
+error_df = nrow(data_new) - number_of_parameters_estimated
+2*pt(abs(tstat), df=error_df, lower.tail = FALSE)
+
